cef25b2106c5a6b7257471278c53009be54dc784
jmarcantelli <jmarcantelli@ipipeline.com>
2016-07-25 14:23:19 -0600

    Reset tables back

--- Tables/dbo.Email.sql
+++ Tables/dbo.Email.sql
@@ -1,8 +1,7 @@
 CREATE TABLE [dbo].[Email]
 (
 [EmailID] [int] NOT NULL IDENTITY(1, 1),
-[EmailAddress] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-[SecondAddress] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[EmailAddress] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Email] ADD CONSTRAINT [PK_Email] PRIMARY KEY CLUSTERED  ([EmailID]) ON [PRIMARY]
