faffa3b8330bd7deb908b2e731d6fd6e87cb2793
jmarcantelli <jmarcantelli@ipipeline.com>
2016-07-25 13:13:51 -0600

    Add field

--- Tables/dbo.Email.sql
+++ Tables/dbo.Email.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[Email]
 (
 [EmailID] [int] NOT NULL IDENTITY(1, 1),
-[EmailAddress] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+[EmailAddress] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+[SecondAddress] [nvarchar] (200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Email] ADD CONSTRAINT [PK_Email] PRIMARY KEY CLUSTERED  ([EmailID]) ON [PRIMARY]
